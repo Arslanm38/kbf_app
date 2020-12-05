824b512e295a72bf0f3c9381bd4800197c19adec
LaurenceKBF <75386109+LaurenceKBF@users.noreply.github.com>
2020-12-05 11:57:35 +0100

    Sidebar Integration
    Derzeit nur über extra button und extern.

---
+++ app/src/main/java/de/hsh/f4/mc/kbfapp/NaviFragment.java
@@ -0,0 +1,28 @@
+package de.hsh.f4.mc.kbfapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NaviFragment extends Fragment {
+
+
+    public NaviFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navi, container, false);
+    }
+}
