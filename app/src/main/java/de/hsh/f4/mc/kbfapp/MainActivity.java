a572ac3568ea15764d1c9b3e1c536dd39fb469d9
LaurenceKBF <75386109+LaurenceKBF@users.noreply.github.com>
2020-12-09 15:13:59 +0100

    Login Switch implementiert. Fahrer Login gelöscht.

--- app/src/main/java/de/hsh/f4/mc/kbfapp/MainActivity.java
+++ app/src/main/java/de/hsh/f4/mc/kbfapp/MainActivity.java
@@ -1,34 +1,78 @@
 package de.hsh.f4.mc.kbfapp;
-/* Erstellt von David Medic*/
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import org.w3c.dom.Text;
 
 import static de.hsh.f4.mc.kbfapp.R.*;
 
+//Laurence Brenner
+
 public class MainActivity extends AppCompatActivity {
 
+    SwitchCompat LoginSwitch;
+    TextView LoginText;
+    Button LoginButtonFahrer, LoginButtonUnternehmer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    protected void onCreate (Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
         setContentView(layout.activity_main);
+
+        LoginSwitch = findViewById(id.switch2);
+        LoginText = findViewById(id.textView3);
+        LoginButtonFahrer = findViewById(id.buttonLoginFahrer);
+        LoginButtonUnternehmer = findViewById(id.buttonLoginUnternehmer);
+
+        LoginSwitch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (LoginSwitch.isChecked()) {
+                    LoginText.setText("Unternehmer-Login");
+                    LoginSwitch.setText("Für Fahrer");
+                    LoginButtonFahrer.setVisibility(View.INVISIBLE);
+                    LoginButtonUnternehmer.setVisibility(View.VISIBLE);
+
+
+                }
+
+                else {
+                    LoginText.setText("Fahrer-Login");
+                    LoginSwitch.setText("Für Unternehmer");
+                    LoginButtonFahrer.setVisibility(View.VISIBLE);
+                    LoginButtonUnternehmer.setVisibility(View.INVISIBLE);
+
+
+                }
+            }
+        });
     }
 
-    public void oeffneRegistrierung(View view) {
-        startActivity(new Intent(MainActivity.this, Registrierung.class));
+    public void oeffneFahrerStartseite(View view) {
+        startActivity(new Intent(MainActivity.this, FahrerStartseite.class));
     }
+
+    public void oeffneSidebarTest (View view) {
+        startActivity(new Intent(MainActivity.this, SidebarTest.class));
+    }
+
+    //David Medic
 
     public void oeffneUnternehmerStartseite(View view) {
         startActivity(new Intent(MainActivity.this, UnternehmerStartseite.class));
     }
 
-    public void wechsleFahrerLogin(View view) {
-        startActivity(new Intent(MainActivity.this, FahrerLogin.class));
+    public void oeffneRegistrierung(View view) {
+        startActivity(new Intent(MainActivity.this, Registrierung.class));
     }
-
 }
-/* Erstellt von David Medic*/
