5edd672e1048262db0fc8a31219d930b6e66fec2
LaurenceKBF <75386109+LaurenceKBF@users.noreply.github.com>
2020-12-20 21:34:12 +0100

    Fix

---
+++ app/src/main/java/de/hsh/f4/mc/kbfapp/UnternehmerFahrerkonten.java
@@ -0,0 +1,14 @@
+package de.hsh.f4.mc.kbfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UnternehmerFahrerkonten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unternehmer_fahrerkonten);
+    }
+}
