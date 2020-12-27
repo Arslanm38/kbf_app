adce778f3a22d57e5a5b390676f79fd4374218da
LaurenceKBF <75386109+LaurenceKBF@users.noreply.github.com>
2020-12-27 13:53:25 +0100

    MainActivity überarbeitet.

--- app/src/main/java/de/hsh/f4/mc/kbfapp/Registrierung.java
+++ app/src/main/java/de/hsh/f4/mc/kbfapp/Registrierung.java
@@ -57,111 +57,101 @@ public class Registrierung extends AppCompatActivity {
         fStore = FirebaseFirestore.getInstance();
 
 
-        // Weiterleitung für angemeldete User
-
-                                        /*
-                                        if(fAuth.getCurrentUser() != null) {
-                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                            finish();
-                                        }
-                                        */
-
         // Registrierung
-        rButtonReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = rEmail.getText().toString().trim();
-                String passwort = rPasswort.getText().toString().trim();
-                String passwortBes = rPasswortBes.getText().toString().trim();
-                String strasse = rStrasse.getText().toString();
-                String plz = rPlz.getText().toString();
-                String name = rName.getText().toString();
-                Boolean isFahrer = rIsFahrer.isChecked();
-                Boolean isUnternehmer = rIsUnternehmer.isChecked();
+        rButtonReg.setOnClickListener(v -> {
+            String email = rEmail.getText().toString().trim();
+            String passwort = rPasswort.getText().toString().trim();
+            String passwortBes = rPasswortBes.getText().toString().trim();
+            String strasse = rStrasse.getText().toString();
+            String plz = rPlz.getText().toString();
+            String name = rName.getText().toString();
+            Boolean isFahrer = rIsFahrer.isChecked();
+            Boolean isUnternehmer = rIsUnternehmer.isChecked();
 
-                if (!(isFahrer || isUnternehmer)) {
-                    Toast.makeText(Registrierung.this, "Bitte Kontotyp angeben", Toast.LENGTH_SHORT).show();
-                }
-
-                if(TextUtils.isEmpty(name)) {
-                    rName.setError("Name wird benötigt");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(email)) {
-                    rEmail.setError("E-Mail Adresse benötigt");
-                    return;
-                }
-
-                if(!(passwort.equals(passwortBes))) {
-                    rPasswort.setText(null);
-                    rPasswortBes.setText(null);
-                    rPasswort.setError("Passwörter stimmen nicht überein");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(strasse)) {
-                    rStrasse.setError("Bitte Straße angeben");
-                    return;
-                }
-
-                if(plz.length() != 5) {
-                    rPlz.setError("Die Postleitzahl muss 5-stellig sein");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(passwort)) {
-                    rPasswort.setError("Passwort benötigt");
-                    return;
-                }
-
-                if(passwort.length() < 6) {
-                    rPasswort.setError("Password muss mehr als 6 Zeichen haben");
-                    return;
-                }
-
-                fAuth.createUserWithEmailAndPassword(email,passwort).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(Registrierung.this, "Konto erfolgreich erstellt", Toast.LENGTH_SHORT).show();
-                            userID = fAuth.getCurrentUser().getUid();
-                            DocumentReference documentReference = fStore.collection("users").document(userID);
-
-
-                            Map<String, Object> user = new HashMap<>();
-
-                            user.put("NAME", name);
-                            user.put("PLZ", plz);
-                            user.put("STRASSE", strasse);
-                            user.put("EMAIL", email);
-                            user.put("ISTUNTERNEHMER", isUnternehmer);
-                            user.put("ISTFAHRER", isFahrer);
-
-                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Log.d("TAG", "onSuccess: user Profil erstellt für " + userID);
-                                }
-
-                            });
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }
-
-
-                        else {
-                            Toast.makeText(Registrierung.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+            if (!(isFahrer || isUnternehmer)) {
+                Toast.makeText(Registrierung.this, "Bitte Kontotyp angeben", Toast.LENGTH_SHORT).show();
             }
+
+            if(TextUtils.isEmpty(name)) {
+                rName.setError("Name wird benötigt");
+                return;
+            }
+
+            if(TextUtils.isEmpty(email)) {
+                rEmail.setError("E-Mail Adresse benötigt");
+                return;
+            }
+
+            if(!(passwort.equals(passwortBes))) {
+                rPasswort.setText(null);
+                rPasswortBes.setText(null);
+                rPasswort.setError("Passwörter stimmen nicht überein");
+                return;
+            }
+
+            if(TextUtils.isEmpty(strasse)) {
+                rStrasse.setError("Bitte Straße angeben");
+                return;
+            }
+
+            if(plz.length() != 5) {
+                rPlz.setError("Die Postleitzahl muss 5-stellig sein");
+                return;
+            }
+
+            if(TextUtils.isEmpty(passwort)) {
+                rPasswort.setError("Passwort benötigt");
+                return;
+            }
+
+            if(passwort.length() < 6) {
+                rPasswort.setError("Password muss mehr als 6 Zeichen haben");
+                return;
+            }
+
+            fAuth.createUserWithEmailAndPassword(email,passwort).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(Registrierung.this, "Konto erfolgreich erstellt.", Toast.LENGTH_SHORT).show();
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+
+
+                        Map<String, Object> user = new HashMap<>();
+
+                        user.put("NAME", name);
+                        user.put("PLZ", plz);
+                        user.put("STRASSE", strasse);
+                        user.put("EMAIL", email);
+                        user.put("ISTUNTERNEHMER", isUnternehmer);
+                        user.put("ISTFAHRER", isFahrer);
+
+                        user.put("UNTERNEHMEN", null);
+                        user.put("UNTERNEHMENNUMMER", null);
+
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("TAG", "onSuccess: user Profil erstellt für " + userID);
+                            }
+
+                        });
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+
+
+                    else {
+                        Toast.makeText(Registrierung.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
 
 // /Laurence Brenner
 
-    /* Erstellt von David Medic*/
-
+/* Erstellt von David Medic*/
 
     public void onLogin(View view) {
         finish();
